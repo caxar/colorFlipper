5eb7eb8a52ff6ed80abe337555754af6736d82d4
vasya <ololohkalool@gmail.com>
2020-08-03 11:45:59 +0300

    add code for hex change color in hex.js

--- hex.js
+++ hex.js
@@ -1,8 +1,17 @@
 const currentColor = document.querySelector('.color'),
-	  change = document.getElementById('change'),
+	  changeBtn = document.getElementById('change'),
 	  colors = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
 
 
+changeBtn.addEventListener('click', () => {
+	let hex = '#';
+	for(let i = 0; i < 6; i++) {
+		let random = Math.floor(Math.random()*colors.length);
+		const randomColor = hex += colors[random];
+		currentColor.innerText = randomColor;
+		document.body.style.background = randomColor;
+	}
+});
 
 
 
